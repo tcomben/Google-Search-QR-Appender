ce2345545140c791d4a7ff60b5fcb76f9f0ca290
tcomben <tcomben@gmail.com>
2011-10-13 14:32:16 -0400

    Only append number to nobr elements once

--- js/qrAppend.js
+++ js/qrAppend.js
@@ -3,21 +3,21 @@ var phoneRegex = /\(?\b[0-9]{3}\)?[-. ]?[0-9]{3}[-. ]?[0-9]{4}\b/g;
 jQuery(document).ready(function(){
    var none = searchForPhoneNumbers();
    if(none){
-      setTimeout("searchForPhoneNumbers()", 1000);
+      setTimeout("searchForPhoneNumbers()", 2000);
    }
 });
 
 function searchForPhoneNumbers(){
    var none = true;
+   var usedNums = new Array();
    while((search = phoneRegex.exec(document.body.innerHTML)) != null){      
-      $("nobr").filter(function(i, el) {
-	 var s = search[0].replace("(", "\\(").replace(")", "\\)");
-         return !!$(el).text().match(s);
-      }).append("<br/><img src='http://qrcode.kaywa.com/img.php?s=5&d=TEL%3A" + search[0].replace(/[^0-9\.]+/g, '') + "'/><br/>");
-      none = false;
+      if(usedNums.indexOf(search[0]) == -1){
+         $("nobr").filter(function(i, el) {
+            return !!$(el).text().match(search[0].replace("(", "\\(").replace(")", "\\)"));
+         }).append("<br/><img src='http://qrcode.kaywa.com/img.php?s=5&d=TEL%3A" + search[0].replace(/[^0-9\.]+/g, '') + "'/><br/>");
+         none = false;
+	 usedNums.push(search[0]);
+      }
    }
    return none;
-
 }
-
-//<br/><img src='http://qrcode.kaywa.com/img.php?s=5&d=TEL%3A7345225200/><br/>
