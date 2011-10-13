b844d30aaa537c251ad715f757ff9f43d81c7752
tcomben <tcomben@gmail.com>
2011-10-13 13:32:46 -0400

    Second go at making this work

--- js/qrAppend.js
+++ js/qrAppend.js
@@ -1,10 +1,19 @@
 var phoneRegex = /\(?\b[0-9]{3}\)?[-. ]?[0-9]{3}[-. ]?[0-9]{4}\b/g;
 
-/*jquery(document).ready(function(){
-	while((search = phoneRegex.exec(document.body.innerHTML)) != null){
-	   alert(search);
-	}
-});*/
+jQuery(document).ready(function(){
+   var none = searchForPhoneNumbers();
+   if(none){
+      setTimeout("searchForPhoneNumbers()", 1000);
+   }
+});
 
-document.body.innerHTML = "<div id='qrcode'></div>";
-jquery('#qrcode').qrcode("this plugin is great");
+function searchForPhoneNumbers(){
+   var none = true;
+   var html = document.body.innerHTML;
+   while((search = phoneRegex.exec(document.body.innerHTML)) != null){
+      html = html.replace(search[0], search[0] + "<br/><img src='http://api.qrserver.com/v1/create-qr-code/?data=TEL%3A1" + search[0].replace(/[^0-9\.]+/g, '') + "&size=60x60' style='height: 60px; width: 60px;'><br/>");
+      none = false;
+   }
+   document.body.innerHTML = html;
+   return none;
+}
